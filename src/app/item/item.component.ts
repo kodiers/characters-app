3664da129cb396dbc5c4f8c1d94c524cecd18b91
Victor Yamchinov <kodiers@gmail.com>
2018-04-20 04:26:09 +0300

    added log service and cleanup

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,16 +1,14 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 import { StarWarsService } from '../star-wars.service';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.css'],
-  providers: [StarWarsService],
+  styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
   @Input() character;
-  @Output() sideAssigned = new EventEmitter<{name: string, side: string}>();
   swService: StarWarsService;
 
   constructor(swService: StarWarsService) {
