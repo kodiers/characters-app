3b46587cd1b75b878e9c4e32f6f54fd0c7ff4044
Victor Yamchinov <kodiers@gmail.com>
2018-05-10 04:42:34 +0300

    added http requests

--- src/app/star-wars.service.ts
+++ src/app/star-wars.service.ts
@@ -1,5 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Subject} from 'rxjs/Subject';
+import {Http, Response} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 import {LogService} from './log.service';
 
@@ -11,11 +13,27 @@ export class StarWarsService {
     {name: 'Darth Vader', side: ''},
   ];
   private logService: LogService;
+  http: Http;
 
   charactersChanged = new Subject<void>();
 
-  constructor(logService: LogService) {
+  constructor(logService: LogService, http: Http) {
     this.logService = logService;
+    this.http = http;
+  }
+
+  fetchCharacter() {
+    this.http.get('https://swapi.co/api/people/').map((response: Response) => {
+      const data = response.json();
+      const extractedChars = data.results;
+      const chars = extractedChars.map((char) => {
+        return {name: char.name, side: ''};
+      })
+      return chars;
+    }).subscribe((data) => {
+      console.log(data);
+      this.characters = data;
+    })
   }
 
   getCharacters(chosenList) {
