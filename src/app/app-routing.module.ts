d47da9242cfbd2a4cdb7f5f1b42b7ea1ca58f9a5
Victor Yamchinov <kodiers@gmail.com>
2018-05-15 04:17:28 +0300

    added modules

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {Routes} from '@angular/router';
+import {RouterModule} from '@angular/router';
+
+import {ListComponent} from './list/list.component';
+import {TabsComponent} from './tabs/tabs.component';
+
+const routes: Routes = [
+  {path: 'characters', component: TabsComponent, children: [
+      { path: '', redirectTo: 'all', pathMatch: 'full'},
+      { path: ':side', component: ListComponent }
+    ]},
+  {path: 'new-character', loadChildren: './create-character/create-character.module.ts#CreateCharacterModule'},
+  {path: '**', redirectTo: '/characters'}
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {}
